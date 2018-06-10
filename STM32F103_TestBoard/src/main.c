9906677655cbdd39824bd5fc95084ff7f20151eb
Protological <drewgaylo@gmail.com>
2018-06-10 16:52:34 -0600

    Added gitignore. Commit of IO port changes for STM test

--- STM32F103_TestBoard/src/main.c
+++ STM32F103_TestBoard/src/main.c
@@ -17,14 +17,14 @@
 
 
 // Port numbers: 0=A, 1=B, 2=C, 3=D, 4=E, 5=F, 6=G, ...
-#if 1
+#if 0
 #define BLINK_PORT_NUMBER               (0)
 #define BLINK_PIN_NUMBER                (9)
 #define BLINK_ACTIVE_LOW                (0)
 #else
-#define BLINK_PORT_NUMBER               (0)
-#define BLINK_PIN_NUMBER                (10)
-#define BLINK_ACTIVE_LOW                (1)
+#define BLINK_PORT_NUMBER               (2)
+#define BLINK_PIN_NUMBER                (4)
+#define BLINK_ACTIVE_LOW                (0)
 #endif
 
 #define BLINK_GPIOx(_N)                 ((GPIO_TypeDef *)(GPIOA_BASE + (GPIOB_BASE-GPIOA_BASE)*(_N)))
